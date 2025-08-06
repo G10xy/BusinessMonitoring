6a76faaa641b188b13ee576208f70e77b21aebf4
gioxi <desiderio.gio@yahoo.com>
2025-08-06 12:00:56 +0200

    renaming main class application

--- src/main/java/it/aruba/BusinessMonitoringApplication.java
+++ src/main/java/it/aruba/BusinessMonitoringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoTestingApplication {
+public class BusinessMonitoringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoTestingApplication.class, args);
+		SpringApplication.run(BusinessMonitoringApplication.class, args);
 	}
 
 }
