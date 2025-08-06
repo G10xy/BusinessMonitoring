b5229c3e2ffa44751a6abe1d96f9cff028a9a060
gioxi <desiderio.gio@yahoo.com>
2025-08-06 21:40:54 +0200

    add custom processing file exception

---
+++ src/main/java/it/aruba/model/FileProcessingException.java
@@ -0,0 +1,11 @@
+package it.aruba.model;
+
+public class FileProcessingException extends RuntimeException {
+    public FileProcessingException(String message) {
+        super(message);
+    }
+
+    public FileProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
