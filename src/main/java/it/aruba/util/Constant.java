d1e8dabf8f93f1f9f5946af5d5c7c73c67370e24
gioxi <desiderio.gio@yahoo.com>
2025-08-14 18:39:53 +0200

    add dto for csv row mapping

---
+++ src/main/java/it/aruba/util/Constant.java
@@ -0,0 +1,19 @@
+package it.aruba.util;
+
+public class Constant {
+
+    public static final String MIME_TYPE_TEXT_CSV = "text/csv";
+    public static final String MIME_TYPE_TEXT_PLAIN = "text/plain";
+    public static final String MIME_TYPE_APPLICATION_CSV = "application/csv";
+    public static final String MIME_TYPE_APPLICATION_VND_MS_EXCEL = "application/vnd.ms-excel";
+
+    public static final String EXTENSION_CSV = ".csv";
+    public static final String EXTENSION_TXT = ".txt";
+
+    public static final String HEADER_CUSTOMER_ID = "customer_id";
+    public static final String HEADER_SERVICE_TYPE = "service_type";
+    public static final String HEADER_ACTIVATION_DATE = "activation_date";
+    public static final String HEADER_EXPIRATION_DATE = "expiration_date";
+    public static final String HEADER_AMOUNT = "amount";
+    public static final String HEADER_STATUS = "status";
+}
