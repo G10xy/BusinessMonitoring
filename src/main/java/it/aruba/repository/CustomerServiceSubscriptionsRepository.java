3fbf811a39482fd893035b13ea4dd712f011b8ec
gioxi <desiderio.gio@yahoo.com>
2025-08-17 19:48:39 +0200

    update

--- src/main/java/it/aruba/repository/CustomerServiceSubscriptionsRepository.java
+++ src/main/java/it/aruba/repository/CustomerServiceSubscriptionsRepository.java
@@ -1,9 +1,9 @@
 package it.aruba.repository;
 
-import it.aruba.model.projection.AvgCustomerSpending;
-import it.aruba.model.projection.ServiceTypeCount;
 import it.aruba.model.entity.CustomerServiceSubscriptions;
 import it.aruba.model.enums.SubscriptionStatusEnum;
+import it.aruba.model.projection.AvgCustomerSpending;
+import it.aruba.model.projection.ServiceTypeCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
